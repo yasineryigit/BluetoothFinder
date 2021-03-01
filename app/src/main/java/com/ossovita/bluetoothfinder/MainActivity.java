2ed2830be28fe920823fecd4d2c0cb564439a184
sancaryum <yasineryigit737@gmail.com>
2021-03-01 16:17:18 +0300

    Searching on bluetooth devices is done

--- app/src/main/java/com/ossovita/bluetoothfinder/MainActivity.java
+++ app/src/main/java/com/ossovita/bluetoothfinder/MainActivity.java
@@ -3,7 +3,13 @@ package com.ossovita.bluetoothfinder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -15,6 +21,27 @@ public class MainActivity extends AppCompatActivity {
     TextView statusTextView;
     Button searchButton;
     BluetoothAdapter bluetoothAdapter;
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            Log.i("Action",action);
+
+            //Arama bittiğinde action objesi finished şeklinde bir değer döndürür
+            if(BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)){//yani arama bittiğinde
+                statusTextView.setText("Finished");
+                searchButton.setEnabled(true);
+            }else if (BluetoothDevice.ACTION_FOUND.equals(action)){//yeni bulunan cihazı device objesine at
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String name = device.getName();
+                String address = device.getAddress();
+                String rssi = Integer.toString(intent.getShortExtra(BluetoothDevice.EXTRA_RSSI,Short.MIN_VALUE));
+                Log.i("Device found","Name: "+ name + " address: " + address+ " RSSI:" + rssi);
+            }
+
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +49,22 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.listView);
         statusTextView = findViewById(R.id.statusTextView);
         searchButton = findViewById(R.id.searchButton);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+        intentFilter.addAction(BluetoothDevice.ACTION_FOUND);
+        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(broadcastReceiver,intentFilter);
     }
 
     public void search(View v){
         statusTextView.setText("Searching...");
         searchButton.setEnabled(false);
+
+
+        bluetoothAdapter.startDiscovery();
+
     }
 }
